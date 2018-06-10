a5f0918735fcec1710fa1586b2c5a75210b141df
Mary Jack <maryjackinthebox@gmail.com>
2018-06-10 19:29:39 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,6 +2,9 @@ require 'pry'
 
 class Artist
   extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Paramable::InstanceMethods
+  include Memorable::InstanceMethods
   attr_accessor :name
   attr_reader :songs
 
