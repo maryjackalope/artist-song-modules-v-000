26f61bba408b09fe7f83bde4f388f2767c97ac41
Mary Jack <maryjackinthebox@gmail.com>
2018-06-10 19:35:32 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,8 +5,6 @@ class Song
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
   
-  include Paramable::InstanceMethods
-  include Memorable::InstanceMethods
   
   attr_accessor :name
   attr_reader :artist
